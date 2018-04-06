2b31bd8aebbf5fac2a341ec30e30e8205215362b
Jie Meng <jiemeng@thoughtworks.com>
2018-04-07 01:10:25 +0800

    Remove useless code

--- plotlib/vps.py
+++ plotlib/vps.py
@@ -20,15 +20,3 @@ class VPS(object):
 
     def getAttrs(self):
         return list(self.__data.keys())
-
-if __name__ == "__main__":
-    vps = VPS('aws.t2.micro')
-    vps.setData('Dhrystone 2 using register variables', 2767.7)
-    vps.setData('Double-Precision Whetstone', 707.5)
-    vps.setData('Execl Throughput', 914.8)
-    vps.setData('File Copy 1024 bufsize 2000 maxblocks', 1720.8)
-    vps.setData('File Copy 256 bufsize 500 maxblocks', 1070.4)
-    vps.setData('File Copy 4096 bufsize 8000 maxblocks', 4012.2)
-    vps.setData('Pipe Throughput', 764.5)
-
-    print(vps.getAttrs())
